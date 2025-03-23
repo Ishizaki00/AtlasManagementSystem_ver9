d9ee84a4fb9fc740c58f5d33841c3b00ee632da3
石崎貴幸 <>
2025-03-23 17:06:12 +0900

    初期データ登録

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Users\User;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        User::create([
+        'over_name' => '石﨑',
+        'under_name' => '貴幸',
+        'over_name_kana' => 'イシザキ',
+        'under_name_kana' => 'タカユキ',
+        'mail_address' => 'griranse@gmail.com',
+        'sex' => '1',
+        'birth_day' => '1998-01-06',
+         'role' => '1',
+        'password' => bcrypt('zef2efn7'), // ハッシュ化されたパスワード
+    ]);
     }
 }
