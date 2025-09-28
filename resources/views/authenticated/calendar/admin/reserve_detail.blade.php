59c353e391f76b94a30f086d3768c2a4bd18a018
石崎貴幸 <>
2025-09-28 19:02:23 +0900

    予約詳細ページ

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,17 +1,31 @@
 <x-sidebar>
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
-    <p><span>日</span><span class="ml-3">部</span></p>
+    <p><span>{{ $date}}日</span><span class="ml-3">{{ $part}}部</span></p>
     <div class="h-75 border">
-      <table class="">
+      <table class="w-100">
         <tr class="text-center">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
+          <th class="w-25">場所</th>
         </tr>
-        <tr class="text-center">
-          <td class="w-25"></td>
-          <td class="w-25"></td>
-        </tr>
+        @php $total = 0; @endphp
+        @foreach($reservePersons as $setting)
+          @foreach($setting->users as $user)
+            @php $total++; @endphp
+            <tr class="text-center">
+              <td class="w-25">{{ $user->id }}</td>
+              <td class="w-25">{{ $user->over_name ?? '' }} {{ $user->under_name ?? '' }}</td>
+              <td class="w-25">{{ $user->place ?? 'リモート' }}</td>
+            </tr>
+          @endforeach
+        @endforeach
+
+        @if($total === 0)
+          <tr>
+            <td colspan="3" class="text-center text-muted">予約者はいません</td>
+          </tr>
+        @endif
       </table>
     </div>
   </div>
